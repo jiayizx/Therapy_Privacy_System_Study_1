0a0e08b7bbc26ded2610c5f3068186e8b1b0c371
PraphulSamavedam <praphulsamavedam@gmail.com>
2024-11-23 18:24:28 -0500

    Makes the url as parameter driven.

--- webapp/post_survey_3.py
+++ webapp/post_survey_3.py
@@ -44,7 +44,7 @@ def update_selected_options():
 
 def close_and_redirect(url=PROLIFIC_URL):
     """Close the current tab after 5 seconds."""
-    st.link_button("Back to the Prolific and complete the task!", "https://app.prolific.co/submissions/complete?cc=CWU9VX3E")
+    st.link_button("Back to the Prolific and complete the task!", url)
 
 
 def post_survey_three():
@@ -169,4 +169,3 @@ def post_survey_three():
 
             # Close the current tab after 5 seconds
             close_and_redirect()
-            # st.rerun()
